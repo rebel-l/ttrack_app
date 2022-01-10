be1d1c8c13b179394ff98f73e0ade44c4ddbd10a
Lars Gaubisch <l.gaubisch@googlemail.com>
2022-01-10 23:47:21 +0100

    played around with some type script components

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import FirstComponent from "./copmonents/FirstComponent";
+import UserComponent from "./copmonents/UserComponent";
+
 ReactDOM.render(
     <div>
         <h1>Hello, Welcome to React and TypeScript</h1>
+        <FirstComponent />
+        <UserComponent name="John Doe" age={25} address="87 Summer St, Boston, MA 02110" dob={new Date()} />
     </div>,
     document.getElementById("root")
 );

5f4f5ac9eb22d8b5f9a01bd879e4fd9cd6b94afe
Lars Gaubisch <l.gaubisch@googlemail.com>
2022-04-02 22:51:38 +0200

    changed api URI

--- src/service/timelogs.ts
+++ src/service/timelogs.ts
@@ -9,7 +9,7 @@ export interface SaveFunc {
 
 export const save : SaveFunc = (payload : TimeLog) => async (dispatch) => {
     // eslint-disable-next-line
-    const response = await axios.put("http://localhost:3000/timgelogs", payload); // TODO: create base url dynamic
+    const response = await axios.put("/api/timgelogs", payload); // TODO: create base url dynamic
 
     dispatch(saved(response.data));
 };

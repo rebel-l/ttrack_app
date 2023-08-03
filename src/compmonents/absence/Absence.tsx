f6176b3245ba2c83905dd2c7bef742f53b4e4d8b
Lars Gaubisch <l.gaubisch@googlemail.com>
2023-08-04 01:02:37 +0200

    added Absence component and model

--- src/compmonents/absence/Absence.tsx
+++ src/compmonents/absence/Absence.tsx
@@ -1,9 +1,94 @@
 import React from "react";
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
+import '@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import { Button, Form } from "react-bootstrap";
+import { Reasons, Vacation } from "../../models/Absence";
 
-class Abesence extends React.Component<any, any> {
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+interface IState {
+    range: Value;
+    reason: string;
+    reasonOptions: React.ReactNode[];
+}
+
+const
+    labelReason = "Reason",
+    labelAdd = "Add";
+class Abesence extends React.Component<any, IState> {
+    constructor (props: any) {
+        super(props);
+
+        const reasonOptions: React.ReactNode[] = [];
+
+        Reasons.forEach((value: string) => {
+            const item = (
+                <option key={value}>
+                    {value}
+                </option>
+            );
+
+            reasonOptions.push(item);
+        });
+
+        // Initial state
+        this.state = {
+            range: [new Date(), new Date()] as Value,
+            reason: Vacation,
+            reasonOptions
+        };
+
+        // Bindings
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleReason = this.handleReason.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    handleAdd(){
+        // TODO: create working days for range and send to backend
+        console.log(this.state);
+        console.log("ADD");
+    }
+
+    handleReason (elem: React.FormEvent<HTMLSelectElement>): void {
+        this.setState({ reason: elem.currentTarget.value });
+    }
+
+    onChange(data: Value){
+        this.setState({ range: data });
+    }
     render () {
+        const { reasonOptions } = this.state
+
         return (
-            <h2>Absence</h2>
+            <div className="justify-content-center">
+                <div>
+                    <span>
+                        {labelReason}
+                    </span>
+                    <Form.Select
+                        onChange={this.handleReason}
+                        size="sm"
+                        defaultValue={this.state.reason}
+                    >
+                        {reasonOptions}
+                    </Form.Select>
+                </div>
+                <div>
+                    <DateRangePicker format={"dd.MM.y"} onChange={this.onChange} value={this.state.range} />
+                </div>
+                <div className="justify-content-center">
+                    <Button
+                        onClick={this.handleAdd}
+                        size="lg"
+                        variant={"primary"}
+                    >
+                        {labelAdd}
+                    </Button>
+                </div>
+            </div>
         );
     }
 }
---
+++ src/models/Absence.ts
@@ -0,0 +1,6 @@
+export const SickLeave = "sick leave",
+    Vacation = "vacation",
+    Reasons : readonly string[] = [
+        SickLeave,
+        Vacation,
+    ];
